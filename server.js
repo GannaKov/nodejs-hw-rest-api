c0ecf3431d121d3339ea3149b91aca4d9c4f265c
GannaKov <zlatta2000@gmail.com>
2023-03-03 13:37:50 +0100

    start, mongoose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// ------------------------------
+const DB_HOST =
+  "mongodb+srv://Zlatta:Zlatta13061973@cluster0.bvmffia.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
